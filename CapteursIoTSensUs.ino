e2aa82cb0e54b6983ef07e5700f0ac2b63d05e68
Arthur Pochart <62388835+arthurpochart@users.noreply.github.com>
2022-04-07 12:49:45 +0200

    Added buffer length for bluetooth api requests

--- CapteursIoTSensUs.ino
+++ CapteursIoTSensUs.ino
@@ -10,18 +10,15 @@ void motd();
 //----Buetooth-----------//
 int incomingByte = 0;
 
-
 int alagogPin = 18;
 int potVal = 0;
 char dta[20];
 int n;
-String TRA,OBJ,REQ,TYP,NUM,VAL,TIM,CHK;
+String TRA, OBJ, REQ, TYP, NUM, VAL, TIM, CHK;
 
 //Variables de trame//
 
-           // TRA  OBJ   REQ TYP  NUM   VAL   TIM    CHK
-
-
+// TRA  OBJ   REQ TYP  NUM   VAL   TIM    CHK
 
 void setup() {
   Serial.begin(9600); // Initialise le port s├®rie ├á 9600 bps
@@ -32,26 +29,6 @@ void setup() {
   Serial.print("R0: ");
   Serial.println(rzero); // Valeur ├á reporter ligne 27 du fichier mq135.h apr├¿s 48h de pr├®chauffage
   motd();
-
-  dta[0] = '1';
-  dta[1] = '0';
-  dta[2] = '0';
-  dta[3] = '0';
-  dta[4] = '1';
-  dta[5] = '1';
-  dta[6] = '1';
-  dta[7] = '1';
-  dta[8] = '1';
-  dta[9] = '1';
-  dta[10] = '1';
-  dta[11] = '1';
-  dta[12] = '1';
-  dta[13] = '1';
-  dta[14] = '1';
-  dta[15] = '1';
-  dta[16] = '1';
-
-
 
 }
 ///---Fonctions-------//
@@ -91,23 +68,57 @@ void pot() {
   delay(5000);
 }
 
-void envoiTrameCourante(){
+void envoiTrameCourante() {
   TRA = "1";
   OBJ = "0001";
   REQ = "1";
   TYP = "1";
   NUM = "01";
-  TIM="0000";
-  CHK="1B";
-  float potVal = analogRead(27);
-  float voltage = potVal * (3.3 / 4094.0);
-  String voltString = String(voltage);
+  TIM = "0000";
+  CHK = "1B";
+  int potVal = analogRead(27);
 
-  String potdata = TRA+OBJ+REQ+TYP+NUM+"002B"+TIM+CHK;
-  
+  String voltString = String(potVal);
+  if (voltString.length() < 4) {
+    String buffer = "";
+    int bufferlength = 4 - voltString.length();
+    for (int i = 0; i < bufferlength; i++) {
+      buffer += "0";
+    }
+    voltString = buffer + voltString;
+
+  }
+  String potdata = TRA + OBJ + REQ + TYP + NUM + voltString + TIM + CHK;
+
   Serial1.print(potdata);
-  Serial.println("Sent: "+ potdata );
+  Serial.println("Sent: " + potdata + " val: " + voltString);
+
+  if (Serial1.available() > 0) {
+    incomingByte = Serial1.read();
+    Serial.println(incomingByte);
+  }
   delay(1000);
+}
+
+String stringToHex(String input) {
+  String hex = “”;
+  for (int i = 0; i < input.length(); i++) {
+    char c = (char) input;
+    hex += String(c, HEX);
+  }
+  return hex;
+}
+String hexToString(String hex) {
+  String text = “”;
+  for (int k = 0; k < hex.length(); k++) {
+    if (k % 2 != 0) {
+      char temp[3];
+      sprintf(temp, "%c%c", hex[k - 1], hex[k]);
+      int number = (int) strtol(temp, NULL, 16);
+      text += char(number);
+    }
+  }
+  return text;
 }
 
 void loop() {
@@ -115,11 +126,6 @@ void loop() {
   boolean running = false;
 
   //--------Interface Utulisateur---------//
-
-
-
-
-
 
   if (Serial.available() > 0) {
     incomingByte = Serial.read();
@@ -140,7 +146,7 @@ void loop() {
       }
 
     }
-     if (incomingByte == 98) {
+    if (incomingByte == 98) {
       running = true;
       while (running) {
         capteurco2();
